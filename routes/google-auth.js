3a0e16018507db2ba9dc4a7a0f63a1e34ce90246
Stefan9541 <stepi08@rambler.ru>
2020-01-18 15:35:21 +0200

    redirect

--- routes/google-auth.js
+++ routes/google-auth.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const passport = require("passport");
+const url =
+  process.env.NODE_ENV === "production"
+    ? "https://good-kino-e6706.firebaseapp.com/"
+    : "http://localhost:3000/";
 
 module.exports = () => {
   router.get(
@@ -16,7 +20,7 @@ module.exports = () => {
       failureRedirect: "/api/logout"
     }),
     (req, res) => {
-      res.redirect(req.header("Referer") || "http://localhost:3000/");
+      res.redirect(req.header("Referer") || url);
     }
   );
 
